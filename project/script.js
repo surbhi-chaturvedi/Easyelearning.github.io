ed229e1e2663623dd5d3155d1478a053cd8b9cbe
surbhi chaturvedi <80679803+surbhi-chaturvedi@users.noreply.github.com>
2022-07-27 13:47:48 +0530

    Delete project directory

--- project/script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-
-if(bar) {
-    bar.addEventListener('click',() => {
-      nav.classList.add('active');
-    })
-}
